4f95eaa883d19a6f3f2c2372105cc53409ac04b6
petrvaneek <99393183+petrvaneek@users.noreply.github.com>
2022-12-07 10:58:04 +0100

    Create main.c

---
+++ project_2/main.c
@@ -0,0 +1,35 @@
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Toggle two LEDs using the internal 8- and 16-bit 
+ *           Timer/Counter.
+ * Returns:  none
+ **********************************************************************/
+int main(void)
+{
+    DDRB = _BV(PB1);
+    // set the comparator registers
+    TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
+    TCCR1B = _BV(CS11);
+    TCCR1A =_BV(COM1A1) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);TCCR1A|= (1<<WGM10);
+   uint_8 no_of
+    OCR1A = 45;
+    OCR1A = 70;
+    
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines, ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
