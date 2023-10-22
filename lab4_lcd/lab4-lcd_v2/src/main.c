e67a0c39d0b2137a3c51138cb0fcdffec36e9fc8
Petr Vaněk <petrvaneek@gmail.com>
2023-10-22 12:27:56 +0200

    lab4 home

--- lab4_lcd/lab4-lcd_v2/src/main.c
+++ lab4_lcd/lab4-lcd_v2/src/main.c
@@ -58,28 +58,50 @@ int main(void)
     lcd_putc('c');
     lcd_gotoxy(11,1);
     lcd_putc('a');*/
-
-    uint8_t customChar[N_CHARS*8] = {
+    /*uint8_t customChar[N_CHARS*8] = {
+        0b00111,
+        0b01110,
+        0b11100,
+        0b11000,
+        0b11100,
+        0b01110,
+        0b00111,
+        0b00011
+    };*/
+  uint8_t customChar [N_CHARS*48] = {
+    // addr 0: .....
+    0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000,
     // addr 1: |....
     0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000,
+    // addr 2:
+    0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000,
+    //addr 3:
+    0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100,0b11100,0b11100,
+    // addr 4:
+    0b11110, 0b11110, 0b11110, 0b11110, 0b11110,0b11110,0b11110, 0b11110,
+    // addr 5:
+    0b11111, 0b11111, 0b11111, 0b11111, 0b11111,0b11111,0b11111, 0b11111
+
+
     };
     // Configuration of 8-bit Timer/Counter2 for Stopwatch update
     // Set the overflow prescaler to 16 ms and enable interrupt
     TIM2_overflow_16ms();
     TIM2_overflow_interrupt_enable();
+    TIM1_overflow_262ms();
+    TIM1_overflow_interrupt_enable();
         // Initialize display
     lcd_init(LCD_DISP_ON);
-
+    
     lcd_command(1<<LCD_CGRAM);       // Set addressing to CGRAM (Character Generator RAM)
                                      // ie to individual lines of character patterns
-    for (uint8_t i = 0; i < N_CHARS*8; i++)  // Copy new character patterns line by line to CGRAM
+    for (uint8_t i = 0; i < N_CHARS*48; i++)  // Copy new character patterns line by line to CGRAM
         lcd_data(customChar[i]);
+    lcd_command(1<<LCD_DDRAM); 
     lcd_command(1<<LCD_DDRAM);       // Set addressing back to DDRAM (Display Data RAM)
                                      // ie to character codes
-
     // Display symbol with Character code 0
-    lcd_putc(0x00);
-
+    //lcd_putc(0x00);
     // Enables interrupts by setting the global interrupt mask
     sei();
 
@@ -100,26 +122,112 @@ int main(void)
  * Purpose:  Update the stopwatch on LCD screen every sixth overflow,
  *           ie approximately every 100 ms (6 x 16 ms = 100 ms).
  **********************************************************************/
-/*ISR(TIMER2_OVF_vect)
-{
+ISR(TIMER2_OVF_vect)
+{   static uint32_t square = 0;
+    static uint8_t symbol = 0;
+    static uint8_t position = 0;
     static uint8_t no_of_overflows = 0;
     static uint8_t tenths = 0;  // Tenths of a second
     static uint8_t seconds = 0;
     static uint8_t minutes = 0;
     char string[2];             // String for converted numbers by itoa()
-
     no_of_overflows++;
-    if (no_of_overflows >= 6)
+    lcd_gotoxy(1+position,1);
+    if (no_of_overflows ==0)
+            {
+                
+                symbol = 0;
+            }
+    else if (no_of_overflows ==1)
     {
+         symbol = 1;
+    }
+    else if (no_of_overflows ==2)
+    {
+         symbol = 2;
+    }
+    else if (no_of_overflows ==3)
+    {
+         symbol = 3;
+    }
+    else if (no_of_overflows ==4)
+    {
+         symbol = 4;
+    }
+    else if (no_of_overflows ==5)
+    {
+         symbol = 5;
+    }
+    
+    lcd_putc(symbol);
+    if (no_of_overflows >= 6)
+    {   
         // Do this every 6 x 16 ms = 100 ms
         no_of_overflows = 0;
         tenths+=1;
+        lcd_gotoxy(1+position, 1);
+        //lcd_putc(symbol);
+        position+=1;
+        //symbol =0;
 
+       /* if (tenths ==2)
+            {
+                
+                symbol = 1;
+            }
+        else if (tenths ==4)
+        {
+            symbol = 2;
+        }
+        else if (tenths ==6)
+        {
+            symbol = 3;
+        }
+        else if (tenths ==8)
+        {
+            symbol = 4;
+        }
+        else if (tenths ==9)
+        {
+            symbol = 5;
+        }
+        else if (tenths ==0)
+        {
+            symbol = 0;
+        }*/
         // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
         if (tenths > 9)
-            {
+            {   symbol=0;
                 tenths = 0;
                 seconds += 1;
+                square = seconds * seconds;
+                itoa(square,string,10);
+                lcd_gotoxy(11,0);
+                lcd_puts(string);
+                position = 0;
+                lcd_gotoxy(1,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(2,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(3,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(4,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(5,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(6,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(7,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(8,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(9,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(10,1);
+                lcd_putc(symbol);
+                lcd_gotoxy(11,1);
+                lcd_putc(symbol);
+
             }
         if (seconds > 59)
             {
@@ -149,4 +257,22 @@ int main(void)
     //GPIO_write_low(&PORTB,PB2);
     // Else do nothing and exit the ISR
 }
-*/
+
+ISR(TIMER1_OVF_vect)
+{   lcd_gotoxy(11,1);
+    uint8_t running_text[] = "   I like Digital electronics!\n";
+    static uint8_t no_of_overflows = 0;
+    static uint8_t text_position = 0;
+    no_of_overflows+=1;
+    if (no_of_overflows>=2)
+        {
+            static uint8_t text_position = 0;
+            lcd_puts(running_text + text_position);
+            size_t length =  strlen(running_text);
+            text_position++;
+            if (text_position >= length) {
+                text_position = 0;
+            }
+        }    
+
+}
